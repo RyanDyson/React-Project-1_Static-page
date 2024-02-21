32a4106453388d511727672eaf9fc3dd28badc1f
Ryan Dyson Darmawan <dysonryan6@gmail.com>
2024-02-21 13:10:38 +0800

    project initialization

---
+++ README.md
@@ -0,0 +1,3 @@
+# Project 1 - Static Page with React
+
+this small project is following the React course on <a href="https://scrimba.com">Scrimba</a>
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import Nav from "./components/Navbar"
+import MainContent from "./components/Main"
+
+function App() {
+    return (
+        <div>
+            <Nav />
+            <MainContent />
+        </div>
+    )
+}
+
+export  default App
